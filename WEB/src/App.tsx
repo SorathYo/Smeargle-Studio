1d043c4c0fac891b67a255449b62e5111b43b0c0
SorathYo <sorathyo@gmail.com>
2026-09-03 17:26:47 -0300

    criando div de conteúdo

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
 
   return (
-    <div></div>  )
+    <div className="flex items-center justify-center h-screen">
+  <div className="w-96 rounded-2xl overflow-hidden shadow-lg">
+    {/* Topo */}
+    <div className="bg-[#8b5e3c] p-4 text-center text-white font-bold">
+      Smeargle Color 🎨
+    </div>
+    {/* Conteúdo */}
+    <div className="bg-[#f5ede0] p-6 text-center text-sm text-[#5c3a18]">
+      Conteúdo
+    </div>
+  </div>
+  </div> )
 }
 
 export default App
